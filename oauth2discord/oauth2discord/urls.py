f60fa4e2da5c01fcbbd4a681951a65c76b3b2057
Radhika Agrawal <74089267+radhika2352@users.noreply.github.com>
2021-04-10 18:56:02 +0530

    Update urls.py

--- oauth2discord/oauth2discord/urls.py
+++ oauth2discord/oauth2discord/urls.py
@@ -4,5 +4,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('oauth2',include('discordlogin.urls'))
+    path('oauth2/',include('discordlogin.urls'))
 ]
